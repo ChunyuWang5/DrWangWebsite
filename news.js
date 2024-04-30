b76c67d9f94f07215ef9c27fbd63509e4f7c29fa
Rafael Cenzano <savagecoder77@gmail.com>
2024-04-30 16:11:21 -0400

    current website

---
+++ news.js
@@ -0,0 +1,254 @@
+var data = {
+  news: [
+    {
+      title: "Dylan's ApoE paper featured on SciTechDaily!",
+      date: "7/24/2023",
+      img_src: "",
+      description:
+        "SciTechDaily reports on our lab's findings of ApoE-heparan sulfate interaction in alzheimer's. ",
+      read_more:
+        "https://scitechdaily.com/new-potential-drug-target-for-alzheimers-disease-discovered/",
+    },
+    {
+      title:
+        "Dylan's ApoE paper featured on RPI News!",
+      date: "5/24/2023",
+      img_src: "",
+      description:
+        "RPI news highlights the lab's work on the ApoE protein and how it modulates alzheimer's risk. ",
+      read_more:
+        "https://news.rpi.edu/content/2023/05/24/rensselaer-researchers-find-new-potential-drug-target-alzheimer%E2%80%99s-disease",
+    },
+    {
+      title:
+        "Our EDG grant featured on RPI News!",
+      date: "9/29/2022",
+      img_src: "",
+      description:
+        "Prof. Chunyu Wang and Prof. Rongjie Lai will make the nuclear magnetic resonance (NMR) of proteins more efficient by utilizing Euclidean Distance Geometry (EDG) and machine learning. ",
+      read_more: "https://news.rpi.edu/approach/2022/09/29",
+    },
+    {
+      title: "EGCG-p53 paper featured on RPI news!",
+      date: "2/12/2021",
+      img_src: "",
+      description:
+        "RPI news describe our work on EGCG-p53 interaction published today in Nature Communications. ",
+      read_more:
+        "https://news.rpi.edu/content/2021/02/12/green-tea-compound-aids-p53-guardian-genome-and-tumor-suppressor",
+    },
+    {
+      title: "EGCG-p53 paper published in Nature Communications!",
+      date: "2/12/2021",
+      img_src: "",
+      description:
+        "In a great team effort, we have discovered a direct interaction between green tea anti-oxidant EGCG and tumor suppressor p53! ",
+      read_more: "https://www.nature.com/articles/s41467-021-21258-5",
+    },
+    {
+      title: "New paper published in Communications Biology!",
+      date: "2/12/2021",
+      img_src: "",
+      description:
+        "In collaboration with Prof. Ding Xu, we have uncovered the unique binding mechanisms between HS and Cystatin-C, a cysteine protease.  ",
+      read_more: "https://doi.org/10.1038/s42003-021-01737-7",
+    },
+    {
+      title: "New R21 funded: a collaboration with Prof. Scott Forth!",
+      date: "1/9/2021",
+      img_src: "",
+      description:
+        "In collaboration with Dr. Scott Forth, we will use single molecule fluorescence to study gamma-secretase. Our congressman Paul Tonko is excited about this project. Read his press release below. ",
+      read_more:
+        "https://tonko.house.gov/news/documentsingle.aspx?DocumentID=3249",
+    },
+    {
+      title:
+        "Our lab received a grant to study mutations that cause Alzheimers",
+      date: "1/8/2021",
+      img_src: "",
+      description:
+        "Our lab received a grant from the National Institute on Aging to study how different genetic mutations affect Alzheimer's. ",
+      read_more:
+        "https://www.timesunion.com/news/article/RPI-wins-grant-to-study-mutations-that-cause-15856357.php",
+    },
+    {
+      title: "New grant, an R01 on Tau-glycan, funded by NIA!",
+      date: "11/10/2020",
+      img_src: "",
+      description:
+        "Our project on tau-glycan interaction in AD has been funded by National Institute on Ageing!",
+      read_more:
+        "https://news.rpi.edu/content/2020/10/28/new-research-aims-combat-prion-spread-tau-pathology-alzheimer’s",
+    },
+    {
+      title: "C1 paper featured on ABC News10!",
+      date: "2/25/2020",
+      img_src: "",
+      description:
+        "TV reporters from ABC news10 interviewed us about our work leading to the discovery of C1 compound for amyloid reduction!",
+      read_more:
+        "https://www.news10.com/news/rpi-researchers-make-new-discovery-in-search-for-alzheimers-cure",
+    },
+    {
+      title: "Our drug discovery research in AD featured on SpectrumNews!    ",
+      date: "2/22/2020",
+      img_src: "",
+      description:
+        "TV reporters from Spectrum news interviewed us about our efforts in AD drug discovery! ",
+      read_more:
+        "https://spectrumlocalnews.com/nys/capital-region/news/2020/02/21/rpi-alzheimer-s-research#",
+    },
+    {
+      title: "AD drug discovery paper published in Chemical Communications! ",
+      date: "2/2/2020",
+      img_src: "",
+      description:
+        "We discovered a novel compound, C1, that targets the substrate of gamma-secretase to reduce amyloid prodcution in both enzyme assays and in cell culture. ",
+      read_more:
+        "https://pubs.rsc.org/en/content/articlelanding/2020/CC/C9CC09170J#!divAbstract",
+    },
+    {
+      title:
+        "Shannon Faris, a 1st year chemistry graduate student, joined our lab!!",
+      date: "12/12/2019",
+      img_src: "",
+      description: "We are very excited that Shannon joins the lab! Welcome! ",
+      read_more: "",
+    },
+    {
+      title: "Our Grant Request is Approved by the Warren Alpert Foundation!",
+      date: "11/20/2019",
+      img_src: "",
+      description:
+        "Our grant request for funding of “Structure-Based Drug Discovery Targeting Substrate of y-Secretiase In Alzhemier’s Disease Project Extension” has been approved by the board of directors of the Warren Alpert Foundation.",
+      read_more: "",
+    },
+    {
+      title: "Dylan is Awarded with the NIGMS Training Program fellowship",
+      date: "11/08/2019",
+      img_src: "",
+      description:
+        "Dylan, one of the graduate students in our lab, was accepted into the NIGMS (National Institute of General Medical Sciences) Training Program fellowship.",
+      read_more: "",
+    },
+    {
+      title: "Jackie’s JACS paper on Hedgehog is published!",
+      date: "11/08/2019",
+      img_src: "images/jacs_9b08914.gif",
+      description:
+        "General Base Swap Preserves Activity and Expands Substrate Tolerance in Hedgehog Autoprocessing",
+      read_more: "https://pubs.acs.org/doi/abs/10.1021/jacs.9b08914",
+    },
+    {
+      title:
+        "Jackie’s Paper Published in Angewandte Chemie as a “very important paper”!",
+      date: "11/07/2019",
+      img_src: "images/anie_201913029.JPG",
+      description:
+        "Rare 3‐O‐sulfation of Heparan Sulfate Enhances Tau Interaction and Cellular Uptake",
+      read_more:
+        "https://onlinelibrary.wiley.com/doi/abs/10.1002/anie.201913029",
+    },
+    {
+      title: "Lauren Joined the Lab as a Graduate Student!",
+      date: "9/27/2019",
+      img_src: "images/LaurenGandy.jpg",
+      description: "",
+      read_more: "",
+    },
+    {
+      title: "We Moved to a New Lab!",
+      date: "9/14/2019",
+      img_src: "images/Lab4.jpg",
+      description: "We moved our lab to CBIS 4332.",
+      read_more: "",
+    },
+    {
+      title: "Adrianne Is Awarded with the NIA AD Training Fellowship",
+      date: "8/11/2019",
+      img_src: "",
+      description:
+        "Adrianne Bunn, one of the graduate students in our lab, was accepted into the NIA (National Institute on Aging) Training Program Fellowship as part of the Alzheimer’s Disease Clinical and Translational Research at Rensselaer! The Training Program provides funding for nine Ph.D. candidates annually and provides interdisciplinary education for predoctoral students.",
+      read_more: "",
+    },
+    {
+      title: "Martina Won First Prize in a Poster Competition!",
+      date: "4/24/2019",
+      img_src: "images/Martina_Poster.jpg",
+      description:
+        "Martina Zhuang, one of our undergraduate lab members, won first prize for her poster at RPI's Undergraduate Research Symposium!",
+      read_more: "",
+    },
+    {
+      title: "Adrianne and Dylan Joined the Lab as Graduate Students!",
+      date: "4/19/2019",
+      img_src: "images/AdrianneDylan.jpg",
+      description: "",
+      read_more: "",
+    },
+    {
+      title: "Bailey Is Awarded with the NIA Training Program Fellowship",
+      date: "4/17/2019",
+      img_src: "",
+      description:
+        "Bailey Eden, one of the graduate students in our lab, was accepted into the NIA (National Institute on Aging) Training Program Fellowship as part of the Alzheimer’s Disease Clinical and Translational Research at Rensselaer! The Training Program provides funding for nine Ph.D. candidates annually and provides interdisciplinary education for predoctoral students.",
+      read_more: "",
+    },
+    {
+      title:
+        "Seeking Candidates for Alzheimer’s Drug to Block Production of Amyloid Peptide",
+      date: "3/1/2019",
+      img_src: "",
+      description:
+        "We are looking for compounds to selectively block the production of amyloid peptide in Alzheimer’s drug discovery",
+      read_more:
+        "https://science.rpi.edu/biology/news/research-seeks-candidates-alzheimer%E2%80%99s-drug-block-production-amyloid-peptide",
+    },
+    {
+      title: "Bailey Joined the Lab!",
+      date: "1/19/2019",
+      img_src: "images/BaileyEden.png",
+      description: "",
+      read_more: "",
+    },
+    {
+      title: "Featured on RPI News: Hedgehog, Cancer, and Zinc",
+      date: "2/27/2017",
+      img_src: "images/Zn Model.png",
+      description:
+        "A team of researchers led by Rensselaer Polytechnic Institute will examine the link between zinc deficiency, Hedgehog, and prostate cancer in a new study funded by the National Institutes of Health (NIH).",
+      read_more:
+        "https://news.rpi.edu/content/2017/02/27/hedgehog-cancer-and-zinc",
+    },
+    {
+      title:
+        "Featured on RPI News: Zinc Deficiency Linked to Activation of Hedgehog Signaling Pathway",
+      date: "4/16/2015",
+      img_src: "images/Hh_signaling.png",
+      description:
+        "Zinc deficiency – long associated with numerous diseases, e.g. autism, lung cancer, prostate cancer, and ovarian cancers – can lead to activation of the Hedgehog signaling pathway.",
+      read_more:
+        "https://news.rpi.edu/content/2015/04/16/zinc-deficiency-linked-activation-hedgehog-signaling-pathway",
+    },
+    {
+      title:
+        "Featured on RPI News: Researchers Uncover Mechanism of Genetic Mutations Known To Cause Familial Alzheimer’s Disease",
+      date: "2014",
+      img_src: "images/gamma_secretase.jpg",
+      description:
+        "New research, led by Rensselaer Polytechnic Institute researcher Chunyu Wang, has solved one mystery in the development of Familial Alzheimer’s Disease (FAD), a genetic variant of the disease that affects a small fraction of the Alzheimer’s population.",
+      read_more:
+        "https://science.rpi.edu/biology/news/researchers-uncover-mechanism-genetic-mutations-known-cause-familial-alzheimer%E2%80%99s",
+    },
+    {
+      title:
+        "Featured on RPI News: Yin and Yang: Balance Could Play Key Role in Progression of Alzheimer's Disease",
+      date: "5/29/2007",
+      img_src: "images/aBetaYinYang.jpg",
+      description:
+        " “We have found that two peptides, Aβ42 and Aβ40, must be in balance for normal function.” ",
+      read_more: "https://news.rpi.edu/luwakkey/2168",
+    },
+  ],
+};
